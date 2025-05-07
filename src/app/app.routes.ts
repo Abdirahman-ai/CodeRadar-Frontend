8574fa1a70a1ce259b13e10e527184b11f0aafd7
Abdirahman-ai <62496069+Abdirahman-ai@users.noreply.github.com>
2025-05-06 19:05:52 -0500

    Generated dashboard, users, projects, and summary/:projectId components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UsersComponent } from './users/users.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContributionSummaryComponent } from './contribution-summary/contribution-summary.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: DashboardComponent },
+    { path: 'users', component: UsersComponent },
+    { path: 'projects', component: ProjectsComponent },
+    { path: 'summary/:projectId', component: ContributionSummaryComponent }
+];
---
+++ src/app/contribution-summary/contribution-summary.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contribution-summary',
+  imports: [],
+  templateUrl: './contribution-summary.component.html',
+  styleUrl: './contribution-summary.component.scss'
+})
+export class ContributionSummaryComponent {
+
+}
