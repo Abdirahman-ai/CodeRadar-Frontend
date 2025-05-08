ec8d1b4404d70b058f3ad6d7c143c1df64fffafd
Abdirahman-ai <62496069+Abdirahman-ai@users.noreply.github.com>
2025-05-07 21:13:42 -0500

    feat: implement project service and display all projects in frontend

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProjectService, Project } from '../services/project.service';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  standalone: true,
+  imports: [NgFor],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit {
+  projects: Project[] = [];
 
+  constructor(private projectService: ProjectService) {}
+
+  ngOnInit(): void {
+    this.projectService.getAllProjects().subscribe((data) => {
+      this.projects = data;
+    });
+  }
 }
--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface Project {
+  id: number;
+  name: string;
+  repoUrl: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectService {
+  private apiUrl = 'http://localhost:8080/api/projects';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getAllProjects(): Observable<Project[]> {
+    return this.http.get<Project[]>(this.apiUrl);
+  }
 }
