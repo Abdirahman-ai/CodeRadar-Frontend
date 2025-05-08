db6f642d32430c621f8a48cdd4b38132d0c1e8ad
Abdirahman-ai <62496069+Abdirahman-ai@users.noreply.github.com>
2025-05-07 21:07:59 -0500

    feat: connect Angular frontend to backend and display users with CORS enabled

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface User {
+  id: number;
+  githubUsername: string;
+  fullName: string;
+  email: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:8080/api/users';
+
+  constructor(private http: HttpClient) {}
+
+  getAllUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.apiUrl);
+  }
 }
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { UserService, User } from '../services/user.service';
 
 @Component({
   selector: 'app-users',
-  imports: [],
+  standalone: true,
+  imports: [NgFor],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss'
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
+  users: User[] = [];
 
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.getAllUsers().subscribe((data) => {
+      this.users = data;
+    });
+  }
 }
